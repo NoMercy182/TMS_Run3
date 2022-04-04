567c3c4af79b28098f03c589a4e3c754087090f3
NoMercy182 <shikaridiazz@gmail.com>
2022-04-05 00:53:07 +0300

    feat: add ToDoApp v.1.2

--- Todo-list/js/script.js
+++ Todo-list/js/script.js
@@ -2,89 +2,198 @@
 
 const root = document.getElementById('root');
 
-const toDoContainer = document.createElement('div');
-toDoContainer.classList.add('container');
+const todoApp = document.createElement('div');
+todoApp.classList.add('appContainer');
 
-// headerUp --------------------------------------------------
+// ------------------------------- navUp ---------------------------------
 
-const toDoHeaderUp = document.createElement('div');
-toDoHeaderUp.classList.add('headerUp');
+const navUp = document.createElement('div');
+navUp.classList.add('navUp');
 
-const buttonDeleteAll = document.createElement('button');
-buttonDeleteAll.classList.add('headerUp__btn');
-buttonDeleteAll.textContent = 'Delete All';
+const btnDeleteAll = document.createElement('button');
+btnDeleteAll.classList.add('navUp__btn', 'btn');
+btnDeleteAll.textContent = 'Delete All';
 
-const buttonDeleteLast = document.createElement('button');
-buttonDeleteLast.classList.add('headerUp__btn');
-buttonDeleteLast.textContent = 'Delete last';
+const btnDeleteLast = document.createElement('button');
+btnDeleteLast.classList.add('navUp__btn', 'btn');
+btnDeleteLast.textContent = 'Delete Last';
 
-const enterToDoField = document.createElement('input');
-enterToDoField.classList.add('headerUp__input');
-enterToDoField.placeholder = 'Enter todo...' ;
+const toDoInput = document.createElement('input');
+toDoInput.classList.add('navUp__input', 'input');
+toDoInput.placeholder = 'Enter To Do ...';
 
-const buttonAdd = document.createElement('button');
-buttonAdd.classList.add('headerUp__btn');
-buttonAdd.textContent = 'Add';
+const btnAdd = document.createElement('button');
+btnAdd.classList.add('navUp__btn', 'btn');
+btnAdd.textContent = 'Add';
 
-// headerDown ------------------------------------------------
+// ------------------------------- navDown ---------------------------------
 
-const toDoHeaderDown = document.createElement('div');
-toDoHeaderDown.classList.add('headerDown');
+const navDown = document.createElement('div');
+navDown.classList.add('navDown');
 
 const counterAll = document.createElement('p');
-counterAll.classList.add('headerDown__counter');
-counterAll.textContent = ' All: 0';
+counterAll.classList.add('navDown__counter1');
+counterAll.innerText = 'All: 0';
 
-const counterCompleated = document.createElement('p');
-counterCompleated.classList.add('headerDown__counter');
-counterCompleated.textContent = 'Completed: 0';
+const counterCompleted = document.createElement('p');
+counterCompleted.classList.add('navDown__counter2');
+counterCompleted.innerText = 'Completed: 0';
 
-const buttonShowAll = document.createElement('button');
-buttonShowAll.classList.add('headerDown__btn');
-buttonShowAll.textContent = 'Show All';
+const btnShowAll = document.createElement('button');
+btnShowAll.classList.add('navDown__btn', 'btn');
+btnShowAll.textContent = 'ShowAll';
 
-const buttonShowCompleated = document.createElement('button');
-buttonShowCompleated.classList.add('headerDown__btn');
-buttonShowCompleated.textContent = 'Show Completed';
+const btnShowCompleted = document.createElement('button');
+btnShowCompleted.classList.add('navDown__btn', 'btn');
+btnShowCompleted.textContent = 'ShowCompleted';
 
-const searchField = document.createElement('input');
-searchField.classList.add('headerDown__input');
-searchField.placeholder = 'Search...';
+const searchInput = document.createElement('input');
+searchInput.classList.add('navDown__input', 'input');
+searchInput.placeholder = 'Search ...';
 
-// ToDoCard ------------------------------------------------
+// ------------------------------- Array ---------------------------------
 
-const toDoCard = document.createElement('div');
-toDoCard.classList.add('card');
+let array = [];
+let arrayComleted = [];
 
-const toDoCardContainerLeft = document.createElement('div');
-toDoCardContainerLeft.classList.add('card__containerLeft');
+// ------------------------------- Functins ---------------------------------
 
-const toDoCardContainerRight = document.createElement('div');
-toDoCardContainerRight.classList.add('card__containerRight');
+const createCard = () => {
+  const text = toDoInput.value;
+  todoApp.append(getToDoCard(text));
+  toDoInput.value = '';
 
-const cardCheckButton = document.createElement('button');
-cardCheckButton.classList.add('card__containerLeft__btn');
-cardCheckButton.innerText = '✓';
+  array = Array.from(array = document.querySelectorAll('.card'));
 
-const cardInput = document.createElement('input');
-cardInput.classList.add('card__containerLeft__input');
-cardInput.placeholder = 'Todo text...';
-
-const cardCloseButton = document.createElement('button');
-cardCloseButton.classList.add('card__containerRight__btn');
-cardCloseButton.innerText = '✗';
-
-const cardDate = document.createElement('p');
-cardDate.classList.add('card__containerRight__date');
-cardDate.textContent = 'Date';
+  console.log(array);
+  counterAll.innerText = `All: ${array.length}`;
+};
 
 
-// append -----------------------------------------------------
-toDoContainer.append(toDoHeaderUp, toDoHeaderDown, toDoCard);
-toDoHeaderUp.append(buttonDeleteAll, buttonDeleteLast, enterToDoField, buttonAdd);
-toDoHeaderDown.append(counterAll, counterCompleated, buttonShowAll, buttonShowCompleated, searchField);
-toDoCard.append(toDoCardContainerLeft, toDoCardContainerRight);
-toDoCardContainerLeft.append(cardCheckButton, cardInput);
-toDoCardContainerRight.append(cardCloseButton, cardDate);
+const deleteAllCard = () => {
+  while (true) {
+    array.pop();
+    if (array.length === 0) {
+      break;
+    }
+  }
+  while (true) {
+    arrayComleted.pop();
+    if (arrayComleted.length === 0) {
+      break;
+    }
+  }
+  document.querySelectorAll('.card').forEach(item => item.remove());
+  counterAll.innerText = `All: ${array.length}`;
+  counterCompleted.innerText = `Completed: ${arrayComleted.length}`;
+};
 
-root.append(toDoContainer);
+const deleteLastCard = () => {
+  document.querySelectorAll('.card')[document.querySelectorAll('.card').length - 1].remove();
+  array.pop();
+  console.log(array.length);
+  console.log(array);
+  counterAll.innerText = `All: ${array.length}`;
+  counterCompleted.innerText = `Completed: ${arrayComleted.length}`;
+};
+
+btnDeleteLast.addEventListener('click', deleteLastCard);
+
+// ------------------------------- card ---------------------------------
+
+function getToDoCard(text) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const cardLeftContainer = document.createElement('div');
+  cardLeftContainer.classList.add('card__LeftContainer');
+
+  const btnCheck = document.createElement('button');
+  btnCheck.classList.add('card__LeftContainer__btn', 'btn', 'btnCkeck');
+  btnCheck.innerText = '';
+  
+// ------------------------------- btnChech ---------------------------------
+
+  btnCheck.addEventListener('click', ()=> {
+    btnCheck.innerText = btnCheck.innerText === '' ? '✓' : '';
+    card.classList.toggle('done');
+    arrayComleted = Array.from(arrayComleted = document.querySelectorAll('.done'));
+    console.log(arrayComleted);
+    counterCompleted.innerText = `Completed: ${arrayComleted.length}`;
+
+  });
+
+  const textArea = document.createElement('p');
+  textArea.classList.add('card__LeftContainer__textArea');
+  textArea.innerText = text;
+
+  const cardRightContainer = document.createElement('div');
+  cardRightContainer.classList.add('card__RightContainer');
+
+  const btnClose = document.createElement('button');
+  btnClose.classList.add('card__RightContainer__btn', 'btn', 'btnClose');
+  btnClose.innerText = '✗';
+
+  const cardDate = document.createElement('p');
+  cardDate.classList.add('card__RightContainer__date');
+  cardDate.innerText = (new Date()).toLocaleString();
+
+  card.append(cardLeftContainer, cardRightContainer);
+  cardLeftContainer.append(btnCheck, textArea);
+  cardRightContainer.append(btnClose, cardDate);
+
+  // ------------------------------- closeThisCard ---------------------------------
+
+  const closeThisCard = () => {
+    if (card.classList.contains('done')) {
+      card.remove();
+      arrayComleted.pop(card);
+    }
+    card.remove();
+    array.pop(card);
+    counterAll.innerText = `All: ${array.length}`;
+    counterCompleted.innerText = `Completed: ${arrayComleted.length}`;
+  };
+
+  btnClose.addEventListener('click', closeThisCard);
+
+  // ------------------------------- showCompletedCards ---------------------------------
+
+  const showCompletedCards = () => {
+    document.querySelectorAll('.card').forEach(item => item.remove());
+    arrayComleted.forEach(item => {
+      todoApp.append(item);
+    });
+  };
+  btnShowCompleted.addEventListener('click', showCompletedCards);
+
+  // ------------------------------- showAllCards ---------------------------------
+
+  const showAllCards = () => {
+    array.forEach(item => {
+      todoApp.append(item);
+    });
+  };
+  btnShowAll.addEventListener('click', showAllCards);
+
+  // ------------------------------- deleteLastCard ---------------------------------
+
+  return card;
+}
+
+// ------------------------------- ClickButton ------------------------------
+
+btnAdd.addEventListener('click', createCard);
+btnDeleteAll.addEventListener('click', deleteAllCard);
+
+// ------------------------------- append -----------------------------------
+
+todoApp.append(navUp, navDown);
+
+navUp.append(btnDeleteAll, btnDeleteLast, toDoInput, btnAdd);
+
+navDown.append(counterAll, counterCompleted, btnShowAll, btnShowCompleted, searchInput);
+
+root.append(todoApp);
+
+// каждому действию приписать новый класс и хуячить его в массив
